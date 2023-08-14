e3ac6a6eb90364b539ab55f8f2487fbcc0214787
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-14 21:37:06 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 console.clear();
 import { generate } from 'rxjs';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/creation/generate
+
 const dotSrc$ = generate(
   2,
   (x) => x <= 38,
@@ -9,3 +11,21 @@ const dotSrc$ = generate(
 );
 
 dotSrc$.subscribe(console.log);
+
+function removeCharacters(inputString, numToRemove) {
+  if (numToRemove >= 0 && numToRemove <= inputString.length) {
+    return inputString.slice(0, -numToRemove);
+  } else {
+    throw new Error('Invalid number of characters to remove');
+  }
+}
+
+// reducing the number of characters
+const dotSrc_2$ = generate(
+  '......................................',
+  (x) => x >= 38,
+  (x) => x - 3,
+  (x) => removeCharacters(x, 3)
+);
+
+dotSrc_2$.subscribe(console.log);
